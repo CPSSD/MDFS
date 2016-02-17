cf7fd6087ea8846e9b9e62b0ced1a9238b0965e2
Michael Wall <thewalla07@gmail.com>
2016-02-17 13:50:22 +0000

    Test case for encryption and decryption

---
+++ utils/encryption_test.go
@@ -0,0 +1,79 @@
+package utils
+
+import "testing"
+
+func TestEnc(t *testing.T) {
+    var tests = []struct {
+        equal bool
+    }{
+        {true}
+    }
+    for _, c := range tests {
+        got := CheckFiles()
+        if got.equal != true {
+            t.Error("Encryption and decryption process failed.")
+        }
+    }
+}
+
+const chunkSize = 4000
+
+func CheckFiles() bool {
+    
+    //test two files for encryption and then decryption
+    source := "/path/to/files/input.txt"
+    encryp := "/path/to/files/input.enc"
+    result := "/path/to/files/result.txt"
+
+    EncryptFile(source, encryp)
+    DecryptFile(encryp, result)
+
+    test1 := CompareFiles(source, result)
+
+    source = "/path/to/files/image.jpg"
+    encryp = "/path/to/files/image.enc"
+    result = "/path/to/files/result.jpg"
+
+    EncryptFile(source, encryp)
+    DecryptFile(encryp, result)
+
+    test2 := CompareFiles(source, result)
+
+    return test1 && test2
+}
+
+func CompareFiles(file1, file2 string) bool {
+    // Check file size ...
+
+    f1, err := os.Open(file1)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    f2, err := os.Open(file2)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for {
+        b1 := make([]byte, chunkSize)
+        _, err1 := f1.Read(b1)
+
+        b2 := make([]byte, chunkSize)
+        _, err2 := f2.Read(b2)
+
+        if err1 != nil || err2 != nil {
+            if err1 == io.EOF && err2 == io.EOF {
+                return true
+            } else if err1 == io.EOF || err2 == io.EOF {
+                return false
+            } else {
+                log.Fatal(err1, err2)
+            }
+        }
+
+        if !bytes.Equal(b1, b2) {
+            return false
+        }
+    }
+}
