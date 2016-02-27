6ca78eb7ad435a2cccb7456877ceb46449335ece
Michael Wall <thewalla07@gmail.com>
2016-02-27 14:56:32 +0000

    Changes to config_test to use new func to get User Home.

--- config/config_test.go
+++ config/config_test.go
@@ -9,10 +9,9 @@ func TestConfig(t *testing.T) {
 	var tests = []struct {
 		filename, protocol, host, port, path string
 	}{
-		{"/path/to/files/config/stnodeconf.json", "tcp", "localhost", "8081", "/path/to/files/stnode/"},
-		{"/path/to/files/config/mdserviceconf.json", "tcp", "localhost", "1994", "/path/to/files/mdservice/"},
-
-
+		{utils.GetUserHome()+"/path/to/files/config/stnodeconf.json", "tcp", "localhost", "8081", "/path/to/files/stnode/"},
+		{utils.GetUserHome()+"/path/to/files/config/mdserviceconf.json", "tcp", "localhost", "1994", "/path/to/files/mdservice/"},
+	}
 	for _, c := range tests {
 		got := ParseConfiguration(c.filename)
 		if got.Path != c.path || got.Host != c.host || got.Port != c.port || got.Protocol != c.protocol {
