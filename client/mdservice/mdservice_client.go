eb58220dd9f2d4666880bfbbc0c43ce531536b29
Michael Wall <thewalla07@gmail.com>
2016-03-04 22:13:27 +0000

    Verbose commenting on the mdservice server side software and client side software. Bugs and notable changes are in the code's comments.

--- client/mdservice/mdservice_client.go
+++ client/mdservice/mdservice_client.go
@@ -3,17 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
-	//"github.com/CPSSD/MDFS/mdservice"
-	//"github.com/CPSSD/MDFS/utils"
 	"net"
 	"os"
 	"strings"
 )
 
-/*
-var currentDir mdservice.DirNode
-var user mdservice.UUID
-*/
 func main() {
 
 	// config will be read locally later
@@ -30,17 +24,30 @@ func main() {
 
 	var sendcode uint8
 
+	// assume we will always start in the root directory (for safety)
 	currentDir := "/"
 
-	//rootDir := mdservice.MkRoot()
-	//currentDir := rootDir
-	//user.Initialise("jim")
-
+	// NOTE: after commenting, I have noticed definite code duplication with regards
+	// to sending the args for a command and sending the sendcode as well. On the server
+	// side this is not as bad as it is here.
+	// NOTE: once methods on server side to do with send/request are implemented, comms
+	// with the storage node can be brought in from the stnode_client.
 	for {
+
+		// get a new reader, perhaps shouldn't do this everytime, although maybe
+		// this will stop user commands from being entered while waiting for results
+		// of commands from the mdservice which could be a good thing.
 		reader := bufio.NewReader(os.Stdin)
+
+		// print the user's command prompt
 		fmt.Print(user + ":" + currentDir + " >> ")
+
+		// read the next command
 		cmd, _ := reader.ReadString('\n')
-		// remove trailing newline character before splitting
+
+		// remove trailing newline character before splitting <- didn't notice this
+		//														 comment before Jacob
+
 		args := strings.Split(strings.TrimSpace(cmd), " ")
 
 		switch args[0] {
@@ -48,6 +55,7 @@ func main() {
 			continue
 
 		case "ls":
+			// START SENDCODE BLOCK
 			sendcode = 1
 
 			err := w.WriteByte(sendcode)
@@ -55,35 +63,52 @@ func main() {
 			if err != nil {
 				panic(err)
 			}
+			// END SENDCODE BLOCK
 
 			// Send current dir
 			w.WriteString(currentDir + "\n")
 			w.Flush()
 
+			// send the length of args
 			err = w.WriteByte(uint8(len(args)))
 			w.Flush()
 			if err != nil {
 				panic(err)
 			}
 
+			// write each arg (if there are any) seperately so that the server
+			// can deal with them as per it's loop
 			for i := 1; i < len(args); i++ {
+
+				// simple sending of args
 				w.WriteString(args[i] + "\n")
 				w.Flush()
 			}
 
+			// read to whitespace? Check corresponding write on the server side
 			msg, _ := r.ReadString(' ')
 
+			// split results by commas
 			files := strings.Split(msg, ",")
 
+			// remove the last newline
 			msg = strings.TrimSuffix(msg, "\n")
 
+			// iterate over each result of the comma separated msg
 			for n, file := range files {
+
+				// don't print the last one? this is hacky code, needs cleaning on
+				// both client and server side, make sure to test changes so as not
+				// to break pieces
 				if n != len(files)-1 {
+
+					// newline for each piece of the result
 					fmt.Println(file)
 				}
 			}
 
 		case "mkdir":
+			// START SENDCODE BLOCK
 			sendcode = 2
 
 			err := w.WriteByte(sendcode)
@@ -91,22 +116,28 @@ func main() {
 			if err != nil {
 				panic(err)
 			}
+			// END SENDCODE BLOCK
 
 			// Send current dir
 			w.WriteString(currentDir + "\n")
 			w.Flush()
 
+			// send len args for mkdir
 			err = w.WriteByte(uint8(len(args)))
 			w.Flush()
 			if err != nil {
 				panic(err)
 			}
 
+			// send each arg (if it exists)
 			for i := 1; i < len(args); i++ {
+
 				w.WriteString(args[i] + "\n")
 				w.Flush()
 			}
+
 		case "rmdir":
+			// START SENDCODE BLOCK
 			sendcode = 3
 
 			err := w.WriteByte(sendcode)
@@ -114,25 +145,34 @@ func main() {
 			if err != nil {
 				panic(err)
 			}
+			// END SENDCODE BLOCK
 
 			// Send current dir
 			w.WriteString(currentDir + "\n")
 			w.Flush()
 
+			// send len args
 			err = w.WriteByte(uint8(len(args)))
 			w.Flush()
 			if err != nil {
 				panic(err)
 			}
 
+			// send each arg (if it exists)
 			for i := 1; i < len(args); i++ {
+
 				w.WriteString(args[i] + "\n")
 				w.Flush()
 			}
 
 		case "cd":
+			// START SENDCODE BLOCK
 			sendcode = 4
 
+			// if the cmd is just "cd", no point telling the server as
+			// the result will be no change made to currentDir
+			// NOTE: here we do not send more than the first arg as any
+			// more than one arg has no effect on results of a cd.
 			if len(args) < 2 {
 				continue
 			}
@@ -142,46 +182,44 @@ func main() {
 			if err != nil {
 				panic(err)
 			}
+			// END SENDCODE BLOCK
 
 			// Send current dir
 			w.WriteString(currentDir + "\n")
 			w.Flush()
 
-			// Send required dir
+			// Send target dir
 			w.WriteString(args[1] + "\n")
 			w.Flush()
 
+			// get response from server if is a dir
 			isDir, _ := r.ReadByte()
 			if isDir == 1 {
 
 				fmt.Println("Not a directory")
 
-			} else {
+			} else { // success!
 
+				// get the new currentDir and clean up the end
 				currentDir, _ = r.ReadString('\n')
 				currentDir = strings.TrimSuffix(currentDir, "\n")
-
 			}
-
-			continue
-			/*
-				err, next := mdservice.Cd(currentDir, args[1])
-				if err != nil {
-					fmt.Println(err)
-				}
-				currentDir = next
-			*/
 
 		case "pwd":
 
+			// no calls to server, just print what we have stored here
 			fmt.Print(currentDir + "\n")
 
-			continue
-
 		case "exit":
+
+			// leave the program. The server will notice that the client has
+			// disconnected and will close the TCP connection on its side
+			// without error.
 			os.Exit(1)
 
 		default:
+
+			// you clearly cannot type correctly
 			fmt.Println("Unrecognised command")
 		}
 	}
--- server/server.go
+++ server/server.go
@@ -111,135 +111,242 @@ func (st StorageNode) handleCode(code uint8, conn net.Conn, r *bufio.Reader, w *
 
 func (md MDService) handleCode(code uint8, conn net.Conn, r *bufio.Reader, w *bufio.Writer) {
 
+	// switch statement for commands
+	// commands work but are not yet optimized, possible code duplication occurs,
+	// although moving some code to a function to reduce duping may not be worth it
+	// if it is only one or two lines.
 	switch code {
 	case 1: // ls
 		fmt.Println("In ls")
 
+		// get current dir
+		// NOTE: here and in other locations, trimming whitespace may be more desirable
 		currentDir, _ := r.ReadString('\n')
 		currentDir = strings.TrimSuffix(currentDir, "\n")
 
+		// get the length of arguments to the ls command
 		lenArgs, _ := r.ReadByte()
 		fmt.Printf("lenArgs = %v\n", lenArgs)
 
+		// the message which will be returned to the user, each entry will be
+		// comma separated for the client to interpret
 		msg := ""
 
+		// if only the ls command was called
 		if lenArgs == 1 {
+
+			// md.getPath() == $HOME/.mdservice/files
+			// NOTE: currentDir should always start with a "/" and end in a normal char,
+			// ie. not a "/". Following this convention avoids occurrences of double
+			// slashes, missing slashes etc.
+
+			// read the contents of the directory location to files
+			// BUG-NOTE: from client, if you cd into a directory, and this directory is
+			// subsequesntly deleted (by os user or otherwise) you will be unable to cd
+			// or ls back up as your currentDir will evaluate to a dir that does not exist.
+			// Possible fix is to do the conversion from relative to absolute path on the
+			// client side (if possible, not sure if it is or not), perhaps failsafe and
+			// return user to home ("/"), or maybe accept that deletions of dirs will
+			// likely not occurr when demoing
 			files, err := ioutil.ReadDir(md.getPath() + currentDir)
 			if err != nil {
 				w.Flush()
 			}
 
+			// iterate over the files, and comma separate them while appending to msg
 			for _, file := range files {
 				msg = msg + file.Name() + ","
 			}
 
 		}
 
+		// loop for dealing with one or more args
 		for i := 1; i < int(lenArgs); i++ {
+
 			fmt.Printf("  in loop at pos %d ready to read\n", i)
 
+			// reading in this arg
 			targetPath, _ := r.ReadString('\n')
 			targetPath = strings.TrimSuffix(targetPath, "\n")
 
-			fmt.Printf("  in loop read in targetPath: %s\n", (currentDir + targetPath))
+			fmt.Printf("  in loop read in targetPath: %s\n", (currentDir + "/" + targetPath))
 
+			// added in a "/" as we do not know if the user tried to call "ls dir" or "ls /dir"
+			// or "ls ./dir" etc. ReadDir() does not mind extra "/"s. "ls /dir" and "ls ./dir"
+			// currently evaluate to the same thing as they are both prepended by currentDir
+			// below, possibly viewed as a bug because it is not identical to UNIX ls.
 			files, err := ioutil.ReadDir(md.getPath() + currentDir + "/" + targetPath)
 			if err != nil {
+
+				// if it is not a directory, skip it and try the next arg
 				continue
+
 			} else {
 
-				msg = msg + targetPath + ":/,"
+				// allows multiple dirs to be ls'd and still know which is which
+				//
+				// ex. output:
+				//
+				// jim:/memes >> ls pepe nyan
+				// pepe:
+				// img1.jpg
+				// img2.png
+				//
+				// nyan:
+				// sound.wav
+				//
+				// jim:/memes >>
+
+				msg = msg + targetPath + ":," // note comma to denote newline
 
 				for _, file := range files {
 					msg = msg + file.Name() + ","
 				}
 
+				// add an extra newline for spacing on client side
 				msg = msg + ","
 			}
 		}
 
+		// remove the last newline for graphical reasons
 		msg = strings.TrimSuffix(msg, ",")
 
+		// add the newline back in for some bizarre reaason? not sure why,
+		// don't remove without testing effects on all types of ls command
 		w.WriteString(msg + ", ")
 		w.Flush()
 
+		// print for terminal's sake
 		fmt.Println("Fin ls")
 
 	case 2: // mkdir
 		fmt.Println("In mkdir")
 
+		// get currentDir
 		currentDir, _ := r.ReadString('\n')
 		currentDir = strings.TrimSuffix(currentDir, "\n")
 
+		// get lenArgs for mkdir
 		lenArgs, _ := r.ReadByte()
 		fmt.Printf("lenArgs = %v\n", lenArgs)
 
+		// if no more than "mkdir" is sent, nothing will happen
+		// if errors occur, the dir will just not be made
 		for i := 1; i < int(lenArgs); i++ {
+
 			fmt.Printf("  in loop at pos %d ready to read\n", i)
+
+			// for each arg, get the target path
 			targetPath, _ := r.ReadString('\n')
+
+			// print the target for terminal's sake
 			fmt.Printf("  in loop read in targetPath: %s", targetPath)
+
+			// MkdirAll creates an entire file path if some dirs are missing
 			os.MkdirAll(md.getPath()+currentDir+"/"+strings.TrimSpace(targetPath), 0777)
 		}
+
+		// end of mkdir
 		fmt.Println("Fin mkdir")
 
 	case 3: // rmdir
 		fmt.Println("In rmdir")
 
+		// get currentDir
 		currentDir, _ := r.ReadString('\n')
 		currentDir = strings.TrimSuffix(currentDir, "\n")
 
+		// get len args for rmdir
 		lenArgs, _ := r.ReadByte()
 		fmt.Printf("lenArgs = %v\n", lenArgs)
 
+		// only does something if more than "rmdir" is called, as above
 		for i := 1; i < int(lenArgs); i++ {
+
 			fmt.Printf("  in loop at pos %d ready to read\n", i)
+
 			targetPath, _ := r.ReadString('\n')
 			fmt.Printf("  in loop read in targetPath: %s", targetPath)
-			os.Remove(md.getPath() + currentDir + strings.TrimSpace(targetPath))
+
+			// this will only remove a dir that is empty, else it does nothing
+			// BUG-NOTE: this command will also currently delete files (there is not
+			// a different command to rmdir an rm in golang), so a check to make sure
+			// the targetPath is a dir should take place (sample code for checking if
+			// a path is a dir or a file is found in "cd" below).
+			// NOTE: a nice to have would be a recursive remove similar to rm -rf,
+			// but this is not needed
+			os.Remove(md.getPath() + currentDir + "/" + strings.TrimSpace(targetPath))
 		}
-		fmt.Println("Fin mkdir")
+
+		// end of rmdir
+		fmt.Println("Fin rmdir")
 
 	case 4: // cd
 		fmt.Println("In cd")
 
+		// get current dir
 		currentDir, _ := r.ReadString('\n')
 		currentDir = strings.TrimSuffix(currentDir, "\n")
 
+		// print for terminal's sake
 		fmt.Printf("currentDir = %s\n", currentDir)
 
+		// get target path
 		targetPath, _ := r.ReadString('\n')
 		targetPath = strings.TrimSuffix(targetPath, "\n")
 
+		// print for terminal's sake
 		fmt.Printf("targetPath = %s\n", targetPath)
 
+		// remove leading and trailing "/"s, not sure if this was necessary,
+		// test before removal for all possible cd commands
 		targetPath = strings.TrimPrefix(targetPath, "/")
 		targetPath = strings.TrimSuffix(targetPath, "/")
 
 		// check if the source dir exist
 		src, err := os.Stat(md.getPath() + currentDir + "/" + targetPath)
-		if err != nil {
+		if err != nil { // not a path ie. not a dir OR a file
+
 			fmt.Println("Path is not a directory")
+
+			// notify the client that it is not a dir with error code "1"
 			w.WriteByte(1)
 			w.Flush()
-		} else {
+
+		} else { // is a path, but is it a dir or a file?
 
 			// check if the source is indeed a directory or not
 			if !src.IsDir() {
+
 				fmt.Println("Path is not a directory")
+
+				// notify the client that it is not a dir with error code "1"
 				w.WriteByte(1)
 				w.Flush()
-			} else {
+
+			} else { // success!
+
+				// notify success to client (no specific code, just not 1 or 0)
 				w.WriteByte(2)
 				w.Flush()
+
+				// create a clean path that the user can display on the cmd line
 				targetPath := path.Join(currentDir + "/" + targetPath)
 				fmt.Printf("Path \"%s\" is a directory\n", targetPath)
+
+				// send the new path back to the user
 				w.WriteString(targetPath + "\n")
 				w.Flush()
 			}
 		}
 
+		// the below cases will entail the logging of a file in the mdservice, telling
+		// the client which storage node to use, where to access files, sending public
+		// keys, permissions, etc.
+
 	case 5: // send
 	case 6: // request
+
 	}
 }
 
@@ -289,15 +396,20 @@ func handleRequest(conn net.Conn, in TCPServer) {
 
 	// var code uint8
 	fmt.Println("Ready to read code")
+
+	// read in the handling code from the connected client
 	code, err := r.ReadByte()
+	// as long as there is no error in the code reading in..
 	for code != 0 {
 
-		fmt.Printf("Read code: %v\n", code)
+		// Print the code to terminal
+		fmt.Printf("Read in code: %v\n", code)
 		in.handleCode(code, conn, r, w)
 
+		// wait to read the next code from the client
 		code, err = r.ReadByte()
-
 	}
 
+	// print when a connection to the client closes along with the error (if any)
 	fmt.Printf("Connection close with code of %v and err of: %v\n", code, err)
 }
